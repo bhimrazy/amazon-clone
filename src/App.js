1f155c492003e6bd8b4a4792f9abc5cb84924451
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-09 00:44:48 +0545

    Clean Up for start done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDPKQLV6f2WnG8r9AhdKlEfbq56Vpm2VEM",
+    authDomain: "clone-1a11b.firebaseapp.com",
+    databaseURL: "https://clone-1a11b.firebaseio.com",
+    projectId: "clone-1a11b",
+    storageBucket: "clone-1a11b.appspot.com",
+    messagingSenderId: "819278953653",
+    appId: "1:819278953653:web:c4e5c0c464437992b952fc",
+    measurementId: "G-RKL8J335V7"
+  };
