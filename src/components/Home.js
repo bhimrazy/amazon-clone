5f5a3287fce8f2b5c5ec7744857899abb391b1d2
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-09 16:01:35 +0545

    making checkout page

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,35 +12,17 @@ function Home() {
         />
         <div className="home__row">
           <Product
-            title="The lean Startup"
-            price={19.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-            rating={5}
+            id="4903850"
+            title="Samsung Gear Fit2 Smartwatch Large, Black"
+            price={59.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            rating={4}
           />
           <Product
-            title="The lean Startup"
-            price={19.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-            rating={5}
-          />
-        </div>
-        <div className="home__row">
-          <Product
-            title="The lean Startup"
-            price={19.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-            rating={5}
-          />
-          <Product
-            title="The lean Startup"
-            price={19.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-            rating={5}
-          />
-          <Product
-            title="The lean Startup"
-            price={19.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+            id="23445930"
+            title="Amazon Echo (3rd Generation) | Smart Speaker with Alexa, Charcoal Fabric"
+            price={49.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/61qDKbBlcgL._AC_SX679_.jpg"
             rating={5}
           />
         </div>
@@ -49,7 +31,28 @@ function Home() {
             title="The lean Startup"
             price={19.99}
             image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+            rating={4}
+          />
+          <Product
+            title="The lean Startup"
+            price={19.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
             rating={5}
+          />
+          <Product
+            title="The lean Startup"
+            price={19.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+            rating={3}
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="98829332"
+            title="The lean Startup"
+            price={149.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/91TrJ3M5EML._AC_SX679_.jpg"
+            rating={4}
           />
         </div>
       </div>
