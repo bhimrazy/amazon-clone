d2480bf708b3eef809889a5341bec2e4638fca45
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-10 01:26:08 +0545

    Login Page created

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout";
+import Login from "./components/Login";
 
 function App() {
   return (
     //BEM
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
--- src/components/Header.js
+++ src/components/Header.js
@@ -17,10 +17,12 @@ function Header() {
         <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest </span>
-          <span className="header__optionLineTwo">SignIn</span>
-        </div>
+        <Link to="/login">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Guest </span>
+            <span className="header__optionLineTwo">SignIn</span>
+          </div>
+        </Link>
         <div className="header__option">
           <span className="header__optionLineOne">Return</span>
           <span className="header__optionLineTwo">& Orders</span>
