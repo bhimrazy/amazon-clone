07c5c6c5c8c271dfb32f4daf462865438c8da32f
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-10 01:47:09 +0545

    Login Page css done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 function Login() {
-  return <div className="login"></div>;
+  return (
+    <div className="login">
+      <Link to="/">
+        <img src="amazon_logo.png" className="login__logo" alt="amazon logo" />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button className="login__signInButton">Sign In</button>
+        </form>
+        <p>
+          By signing you agree to the AMAZON CLONE Conditions of Use & Sale.
+          Please see our Privacy Notice,Or Cookies Notice and Our Internet-Based
+          Ads Notice.
+        </p>
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
