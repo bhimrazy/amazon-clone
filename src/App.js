bb890be314ed1fa54d342384b33d62176d98a654
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-11 21:33:05 +0545

    orders created

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useStateValue } from "./contextapi/StateProvider";
 import Payment from "./components/Payment";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./components/Orders";
 
 const promise = loadStripe(
   "pk_test_51Gxv7bEznFhuF9RkjjTltlbXhZrAsC2hfsmR4bULtO84QjJUVowe85Tmu4qmxE7xO5rU1i3mNxOzGMzh7mBj7iM300lSBnF9QO"
@@ -38,6 +39,9 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Orders />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -7,6 +7,7 @@ import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { getBasketTotal } from "../contextapi/reducer";
 import CurrencyFormat from "react-currency-format";
 import axios from "../axios";
+import { db } from "../firebase";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -49,9 +50,24 @@ function Payment() {
       })
       .then(({ paymentIntent }) => {
         //paymentIntent = payment confirmation
+        db.collection("users")
+          .doc(user?.uid)
+          .collection("orders")
+          .doc(paymentIntent.id)
+          .set({
+            basket: basket,
+            amount: paymentIntent.amount,
+            created: paymentIntent.created,
+          });
+
         setSucceeded(true);
-        setError(false);
+        setError(null);
         setProcessing(false);
+
+        dispatch({
+          type: "EMPTY_BASKET",
+        });
+
         history.replace("/orders");
       });
   };
